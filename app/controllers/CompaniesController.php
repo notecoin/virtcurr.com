f1e4ce12b5f8c0b1ea672e52dff89bb000e9a93e
Nilam Doctor <nilamdoc@gmail.com>
2013-11-09 08:27:18 +0530

    controller

--- app/controllers/CompaniesController.php
+++ app/controllers/CompaniesController.php
@@ -1,15 +1,33 @@
 <?php
 namespace app\controllers;
 use app\models\Countries;
+use app\models\Ipaddresses;
+use app\models\Companies;
 
 class CompaniesController extends \lithium\action\Controller {
 
 	public function index(){
+		$IP = $_SERVER['REMOTE_ADDR'];
+		$ips = explode(".",$IP);
+		$IP_no =  $ips[3] + $ips[2]*256 + $ips[1]*65536 + $ips[0]*16777216;
+		$Country = Ipaddresses::find('all',array(
+			'conditions'=>array(
+				'start_no'=>array('$lte'=>$IP_no), 
+				'end_no'=>array('$gte'=>$IP_no)
+			),
+			'limit'=>1
+		));
+		foreach($Country as $CC){
+			$CountryISO = $CC['ISO'];
+		}
+		$MyCountry = Countries::find('first',array(
+			'conditions'=>array('ISO'=>$CountryISO)
+		));
 
 		$SelectedCountry = Countries::find('first',array(
 		'conditions'=>array('ISO'=>strtoupper(SUBDOMAIN))
 		));
-		return compact('SelectedCountry');
+		return compact('SelectedCountry','MyCountry','IP');
 	}
 }
 ?>
--- app/views/companies/index.html.php
+++ app/views/companies/index.html.php
@@ -1,26 +1,3 @@
-<?php 
-use app\models\Countries;
-use app\models\Ipaddresses;
-use app\models\Companies;
-
-$IP = $_SERVER['REMOTE_ADDR'];
-$ips = explode(".",$IP);
-$IP_no =  $ips[3] + $ips[2]*256 + $ips[1]*65536 + $ips[0]*16777216;
-$Country = Ipaddresses::find('all',array(
-	'conditions'=>array(
-		'start_no'=>array('$lte'=>$IP_no), 
-		'end_no'=>array('$gte'=>$IP_no)
-	),
-	'limit'=>1
-));
-foreach($Country as $CC){
-	$CountryISO = $CC['ISO'];
-}
-$MyCountry = Countries::find('first',array(
-	'conditions'=>array('ISO'=>$CountryISO)
-));
-
-?>
 <br>
 <h4>Companies <small>registered with virtcurr.com </small></h4>
 <div class="row">
